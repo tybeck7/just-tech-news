7de0422b4339d078825c5dbfef9d032f43fa24e7
Tyler Beck <tylerbeck@Tylers-Air.attlocal.net>
2022-03-17 14:24:11 -0700

    password protection

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -47,6 +47,27 @@ router.post('/', (req, res) => {
     });
 });
 
+router.post('/login', (req, res) => {
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  })
+  .then(dbUserData => {
+    if (!dbUserData){
+      res.status(400).json({ message: 'Username does not exists' })
+      return
+    }
+    
+    const validPassword = dbUserData.checkPassword(req.body.password)
+    if (!validPassword){
+      res.status(400).json({ message: 'Password is incorrect' })
+      return
+    }
+    res.json({ user: dbUserData, message: 'You are now logged in' })
+  })
+})
+
 router.put('/:id', (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
